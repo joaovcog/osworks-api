477b49ebc1e456da6ed8f3269f04fe90bfb0962b
joaov <joaov@TC156.alcafoods.org>
2021-03-12 10:58:14 -0300

    Finalizacao do Controller, validando campos e mensagens personalizadas

--- src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -3,16 +3,18 @@ package com.algaworks.osworks.api.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,15 +30,22 @@ public class ClienteController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente adicionar(@RequestBody Cliente cliente) {
+	public Cliente adicionar(@Valid @RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
 	}
 	
-//	@PutMapping("/{codCliente}")
-//	public ResponseEntity<Cliente> atualizar(@PathVariable Long codCliente, 
-//			@ResponseBody Cliente cliente) {
-//		
-//	}
+	@PutMapping("/{codCliente}")
+	public ResponseEntity<Cliente> atualizar(@PathVariable Long codCliente, 
+			@Valid @RequestBody Cliente cliente) {
+		if (!clienteRepository.existsById(codCliente)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		cliente.setCodigo(codCliente);
+		cliente = clienteRepository.save(cliente);
+		
+		return ResponseEntity.ok(cliente);
+	}
 	
 	@GetMapping
 	public List<Cliente> listar() {
@@ -52,6 +61,17 @@ public class ClienteController {
 		}
 		
 		return ResponseEntity.notFound().build();
+	}
+	
+	@DeleteMapping("/{codCliente}")
+	public ResponseEntity<Void> remover(@PathVariable Long codCliente) {
+		if (!clienteRepository.existsById(codCliente)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		clienteRepository.deleteById(codCliente);
+		
+		return ResponseEntity.noContent().build();
 	}
 	
 }
--- src/main/java/com/algaworks/osworks/domain/model/Cliente.java
+++ src/main/java/com/algaworks/osworks/domain/model/Cliente.java
@@ -5,6 +5,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "clientes")
@@ -13,11 +16,18 @@ public class Cliente {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
-
+	
+	@NotBlank
+	@Size(max = 60)
 	private String nome;
-
+	
+	@NotBlank
+	@Email
+	@Size(max = 255)
 	private String email;
 
+	@NotBlank
+	@Size(max = 20)
 	private String telefone;
 
 	public Long getCodigo() {
