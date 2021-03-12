0cc1cc9f6365900a0bc97c86bb9461269350d519
joaov <joaov@TC156.alcafoods.org>
2021-03-12 18:02:50 -0300

    Criacao do model de comentario e do Controller

---
+++ src/main/java/com/algaworks/osworks/api/model/ComentarioModel.java
@@ -0,0 +1,35 @@
+package com.algaworks.osworks.api.model;
+
+import java.time.OffsetDateTime;
+
+public class ComentarioModel {
+
+	private Long codigo;
+	private String descricao;
+	private OffsetDateTime dataEnvio;
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public OffsetDateTime getDataEnvio() {
+		return dataEnvio;
+	}
+
+	public void setDataEnvio(OffsetDateTime dataEnvio) {
+		this.dataEnvio = dataEnvio;
+	}
+
+}
