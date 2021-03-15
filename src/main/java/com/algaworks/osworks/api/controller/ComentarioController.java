ace508a742e4545d510ab49343f77c8a7a159d2b
joaov <joaov@TC156.alcafoods.org>
2021-03-15 10:58:03 -0300

    Criacao e listagem de comentarios para a ordem de servico

--- src/main/java/com/algaworks/osworks/api/controller/ComentarioController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ComentarioController.java
@@ -1,10 +1,14 @@
 package com.algaworks.osworks.api.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.validation.Valid;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,7 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.osworks.api.model.ComentarioInput;
 import com.algaworks.osworks.api.model.ComentarioModel;
+import com.algaworks.osworks.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.osworks.domain.model.Comentario;
+import com.algaworks.osworks.domain.model.OrdemServico;
+import com.algaworks.osworks.domain.repository.OrdemServicoRepository;
 import com.algaworks.osworks.domain.service.GestaoOrdemServicoService;
 
 @RestController
@@ -23,6 +30,9 @@ public class ComentarioController {
 	
 	@Autowired
 	private GestaoOrdemServicoService gestaoOrdemServico;
+	
+	@Autowired
+	private OrdemServicoRepository ordemServicoRepository;
 	
 	@Autowired
 	private ModelMapper modelMapper;
@@ -36,8 +46,22 @@ public class ComentarioController {
 		return toModel(comentario);
 	}
 	
+	@GetMapping
+	public List<ComentarioModel> listar(@PathVariable Long codOrdemServico) {
+		OrdemServico ordemServico = ordemServicoRepository.findById(codOrdemServico)
+				.orElseThrow(() -> new EntidadeNaoEncontradaException("Ordem de serviço não encontrada."));
+		
+		return toCollectionModel(ordemServico.getComentarios());
+	}
+	
 	private ComentarioModel toModel(Comentario comentario) {
 		return modelMapper.map(comentario, ComentarioModel.class);
+	}
+	
+	private List<ComentarioModel> toCollectionModel(List<Comentario> comentarios) {
+		return comentarios.stream()
+				.map(comentario -> toModel(comentario))
+				.collect(Collectors.toList());
 	}
 	
 }
